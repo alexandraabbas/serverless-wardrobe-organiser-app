875133ddcd18bd23350b7b6e706f6458f80e3fb7
Alexandra <alexandra.abbas@datatonic.com>
2019-12-16 14:02:36 +0100

    Add user prefix to request

--- functions/fetch_images/main.py
+++ functions/fetch_images/main.py
@@ -12,23 +12,13 @@ def create_service():
     return googleapiclient.discovery.build("storage", "v1")
 
 
-def get_bucket_metadata(bucket):
-    """Retrieves metadata about the given bucket."""
-    service = create_service()
-
-    # Make a request to buckets.get to retrieve a list of objects in the
-    # specified bucket.
-    req = service.buckets().get(bucket=bucket)
-    return req.execute()
-
-
-def list_bucket(bucket):
+def list_bucket(bucket, user):
     """Returns a list of metadata of the objects within the given bucket."""
     service = create_service()
 
     # Create a request to objects.list to retrieve a list of objects.
     fields_to_return = "nextPageToken,items(name,size,contentType,metadata(my-key))"
-    req = service.objects().list(bucket=bucket, fields=fields_to_return)
+    req = service.objects().list(bucket=bucket, prefix=user, fields=fields_to_return)
 
     all_objects = []
     # If you have too many items to list in one request, list_next() will
@@ -41,13 +31,10 @@ def list_bucket(bucket):
     return all_objects
 
 
-def fetch_image(request):
+def fetch_images(request):
     request_json = request.get_json()
 
     if request_json and "user" in request_json:
         user = request_json["user"]
 
-    print(json.dumps(get_bucket_metadata(BUCKET), indent=2))
-    print(json.dumps(list_bucket(BUCKET), indent=2))
-
-    return json.dumps(get_bucket_metadata(BUCKET), indent=2)
+    return json.dumps(list_bucket(BUCKET, user), indent=2)
